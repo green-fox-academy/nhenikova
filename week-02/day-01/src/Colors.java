50facdb6966b02e6c80c3ddeedd4964a75f5c8a0
Naty <n.henikova@gmail.com>
2019-05-12 13:25:58 +0200

    deleted append a greet, modified colors

--- week-02/day-01/src/Colors.java
+++ week-02/day-01/src/Colors.java
@@ -1,6 +1,6 @@
 public class Colors {
     public static void main(String[] args) {
-
+        String[][] colors = {{"lime", "forest green", "olive", "pale green", "spring green"}, {"orange red", "red", "tomato"}, {"orchid", "violet", "pink", "hot pink"}};
     }
 }
 
