720d2d53bf949160bc74a43966e3d248cfc7bac5
Naty <n.henikova@gmail.com>
2019-05-12 14:03:10 +0200

    modified greet and matrix

--- week-02/day-01/src/Greet.java
+++ week-02/day-01/src/Greet.java
@@ -1,11 +1,13 @@
 public class Greet {
     public static void main(String[] args) {
         String al = "Greenfox";
-        greet(al);
+        System.out.println(greet(al));
     }
 
-    public static void greet(String name) {
-        System.out.println("Greetings dear, " + name);
+    public static String greet(String inputName) {
+        String firstPart = "Greetings dear, ";
+
+        return firstPart + inputName;
     }
 
 }
--- week-02/day-01/src/Matrix.java
+++ week-02/day-01/src/Matrix.java
@@ -3,16 +3,38 @@ import java.util.Scanner;
 
 public class Matrix {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Size of the matrix: ");
-        int matrixSize = scanner.nextInt();
-        int[][] matrix = new int[matrixSize][matrixSize];
-        for (int i = 0; i < matrixSize; i++) {
-            matrix[i][i] = 1;
-            System.out.println(Arrays.toString(matrix[i]));
+        int matrixDimension = 4;
+        int [][] matrix = new int[matrixDimension][matrixDimension];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    matrix [i][j] = 1;
+                } else {
+                    matrix [i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix [i][j]);
+            }
+            System.out.println();
         }
     }
 }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Size of the matrix: ");
+//        int matrixSize = scanner.nextInt();
+//        int[][] matrix = new int[matrixSize][matrixSize];
+//        for (int i = 0; i < matrixSize; i++) {
+//            matrix[i][i] = 1;
+//            System.out.println(Arrays.toString(matrix[i]));
+//        }
+
+
 // - Create (dynamically) a two dimensional array
 //   with the following matrix. Use a loop!
 //
