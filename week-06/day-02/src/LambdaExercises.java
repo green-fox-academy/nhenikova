9de2879f930094169c53594ca616167938b49a2b
Naty <n.henikova@gmail.com>
2019-05-14 15:16:40 +0200

    adding exercises 4, 5, 6

--- week-06/day-02/src/LambdaExercises.java
+++ week-06/day-02/src/LambdaExercises.java
@@ -9,18 +9,18 @@ public class LambdaExercises {
         //Exercise1
         // Write a Stream Expression to get the even numbers
         // from the following list:
-        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14); //first method
         numbers.stream()
                 .filter(n -> n%2 ==0)
                 .forEach(e -> System.out.println(e));
 
-        List<Integer> evenNumbersList;
+        List<Integer> evenNumbersList;                                  //second method
         evenNumbersList = numbers.stream()
                 .filter(n -> n%2 ==0)
                 .collect(Collectors.toList());
         System.out.println(evenNumbersList);
 
-        numbers.stream().filter(n -> n%2 ==0).forEach(System.out::println);
+        numbers.stream().filter(n -> n%2 ==0).forEach(System.out::println); //third method
 
         //Exercise2
         //Write a Stream Expression to get the squared value of
@@ -45,7 +45,29 @@ public class LambdaExercises {
         System.out.println(squaredMoreNumbersList);
 
         //Exercise4
-        //Write a Stream Expression to get the average value of the odd numbers from the following list:
-        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        //Write a Stream Expression to get the average value of the odd
+        //numbers from the following list:
+        double averageOddNumbers;
+        averageOddNumbers = numbers.stream()
+                .filter(n -> !(n%2 ==0))
+                .mapToInt(Integer::intValue)
+                .average()
+                .getAsDouble();
+        System.out.println(averageOddNumbers);
+
+        //Exercise5
+        //Write a Stream Expression to get the sum of
+        //the odd numbers in the following list:
+        List<Integer> numbers3 = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+        int sumOddNumbers;
+        sumOddNumbers = numbers3.stream()
+                .filter(n -> !(n%2 ==0))
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sumOddNumbers);
+
+        //Exercise6
+        //Write a Stream Expression to find the uppercase
+        //characters in a string!
     }
 }
