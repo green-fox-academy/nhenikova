0a1059ecbc834724a362e714d3e4eaa551de816e
Naty <n.henikova@gmail.com>
2019-05-15 15:24:12 +0200

    modified hello web controller, modified greeting.html

--- week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,17 +1,18 @@
 package com.greenfoxacademy.springstart.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWebController {
     @RequestMapping("/web/greeting")
     public String greeting(@RequestParam String name, Model model) {
+        Greeting greetModel = new Greeting("");
         model.addAttribute("name", name);
+        model.addAttribute("counter", greetModel.getId());
         return "greeting";
     }
-
-
 }
