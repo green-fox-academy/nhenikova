bd4118ed6155d4d546b6fbd25920df6b2c79674b
Naty <n.henikova@gmail.com>
2019-05-17 12:03:26 +0200

    modified basic web shopitem and store

--- week-06/day-05/src/main/java/com/greenfoxacademy/basicweb/models/ShopItem.java
+++ week-06/day-05/src/main/java/com/greenfoxacademy/basicweb/models/ShopItem.java
@@ -1,4 +1,54 @@
 package com.greenfoxacademy.basicweb.models;
 
 public class ShopItem {
+    private String name;
+    private String description;
+    private double price;
+    private int quantityOfStock;
+
+    public ShopItem(String name, String description, double price, int quantityOfStock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityOfStock = quantityOfStock;
+    }
+
+    public ShopItem() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantityOfStock() {
+        return quantityOfStock;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void addDescription(String description) {
+        this.description = description;
+    }
+
+    public void addToStock(int quantity) {
+        quantityOfStock += quantity;
+    }
+
+    //@Override
+    //    public int compareTo(ShopItem o) {
+    //        return (int) (this.price - o.getPrice());
+    //    }
 }
+
+
+
--- week-06/day-05/src/main/java/com/greenfoxacademy/basicweb/models/ShopItemStore.java
+++ week-06/day-05/src/main/java/com/greenfoxacademy/basicweb/models/ShopItemStore.java
@@ -1,4 +1,22 @@
 package com.greenfoxacademy.basicweb.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShopItemStore {
+    private List<ShopItem> items;
+
+    public ShopItemStore() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(ShopItem item) {
+        items.add(item);
+    }
+
+    public List<ShopItem> getItems() {
+        return items;
+    }
+
+
 }
