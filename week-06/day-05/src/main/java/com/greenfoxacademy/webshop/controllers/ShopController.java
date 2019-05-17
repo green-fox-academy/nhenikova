54a38cfa6e5cb6cc3064d58f364d3c281b1306f9
Naty <n.henikova@gmail.com>
2019-05-17 15:58:09 +0200

    modified shop controller

--- week-06/day-05/src/main/java/com/greenfoxacademy/webshop/controllers/ShopController.java
+++ week-06/day-05/src/main/java/com/greenfoxacademy/webshop/controllers/ShopController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.webshop.controllers;
 
+import com.greenfoxacademy.webshop.models.ShopItem;
 import com.greenfoxacademy.webshop.models.ShopItemStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ShopController {
-    ShopItemStore shopItemStore = new ShopItemStore();
+    ShopItemStore myStore = new ShopItemStore();
 
-    @RequestMapping ("/web")
-    public String showList(Model model) {
-        model.addAttribute("item", shopItemStore.getItemList());
-        return "webshop";
+    public ShopController() {
+        myStore.addItem(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000.0, 5));
+        myStore.addItem(new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2));
+        myStore.addItem(new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0));
+        myStore.addItem(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119.0, 100));
+        myStore.addItem(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300.0, 1));
     }
+
+    @RequestMapping ("/")
+    public String show(Model model) {
+        model.addAttribute("item", myStore.getItemList());
+        return "myStore";
+    }
+
+    @RequestMapping ("/")
 }
