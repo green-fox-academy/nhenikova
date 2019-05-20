0c92efb3f33d32c9ddb1b08ef477071d5bbf8984
Naty <n.henikova@gmail.com>
2019-05-20 13:51:39 +0200

    creating new bank controller, modified bank account and bank of simba app

--- week-07/day-01/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/app/BankofsimbaApplication.java
+++ week-07/day-01/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/app/BankofsimbaApplication.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy.bankofsimba;
+package com.greenfoxacademy.bankofsimba.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ week-07/day-01/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankController.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.bankofsimba.controller;
+
+public class BankController {
+}
