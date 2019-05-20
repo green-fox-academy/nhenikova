258ce30a0b6ed4e44e402b43a51811452786262f
Naty <n.henikova@gmail.com>
2019-05-20 14:13:14 +0200

    modified bank controller and show.html

--- week-07/day-01/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankController.java
+++ week-07/day-01/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankController.java
@@ -1,4 +1,29 @@
 package com.greenfoxacademy.bankofsimba.controller;
 
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class BankController {
+
+    private List<BankController> bankAccounts;
+
+    public BankController() {
+        this.bankAccounts = new ArrayList<>();
+        //bankAccounts.add(new BankAccount("Simba", 2000.00, "lion"));
+        bankAccounts.add(new BankAccount("", 2000.00, "zebra"));
+    }
+
+    @GetMapping ("/show")
+    public String show(Model model) {
+        BankAccount simba = new BankAccount("Simba", 2000.00, "lion");
+        model.addAttribute("account", simba);
+        return "show";
+    }
 }
